c2b90925989855446e22787b2f8652fa82f931b4
Yariv Gilad <yariv.gilad@gmail.com>
2024-03-25 19:56:32 +0200

    step 1

--- src/view/App.jsx
+++ src/view/App.jsx
@@ -5,11 +5,11 @@ import Counter from "./Counter";
 export default function App() {
   return (
     <div className="app">
-      <TopBar>React Components are state machines</TopBar>
+      <TopBar>React state </TopBar>
       <h1 className="headline white-text">_ ^ _ Stateful Bookstore _ ^ _</h1>
       <Counter item="Book" initialValue={3}/>
-      <Counter item="Customer" initialValue={12}/>
-      <Counter item="Employee" initialValue={24}/>
+      {/* <Counter item="Customer" initialValue={12}/> */}
+      {/* <Counter item="Employee" initialValue={24}/> */}
       <br />
       <hr />
       <NameInput />
--- src/view/TopBar.jsx
+++ src/view/TopBar.jsx
@@ -1,9 +1,46 @@
-import React from "react";
+import styled, {keyframes} from "styled-components";
 
-const TopBar =  ({ children }) => (
-  <div className="top-bar">
+const TopBar = ({ children }) => (
+  <Div>
     <h1>{children}</h1>
     <img className="logo" src="icons/logo.svg" alt="logo" />
-  </div>
+    <h1>Hello</h1>
+  </Div>
 );
 export default TopBar;
+
+const spin = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+`;
+
+const Div = styled.div`
+  background: slategray;
+  width: 100%;
+  height: 70px;
+  padding: 20px;
+  color: white;
+  position: fixed;
+  top: 0;
+  left: 0;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+
+  h1{
+    font-size: 3rem;
+    font-weight: normal;
+    font-family: "Griffy", cursive;
+    color: papayawhip;
+  }
+  img.logo{
+    height: 5rem;
+    animation: ${spin} 2s linear infinite;
+  }
+`;
+
+
